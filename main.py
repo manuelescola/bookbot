885db58422467bd9a8e867a8060b8a0920c5fa95
manuelescola <manuelescolaperez@gmail.com>
2025-02-25 23:17:27 +0000

    Finished the Bookbot

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
-from stats import get_num_words, get_book, get_num_characters
+import sys
+from stats import get_num_words, get_book, get_num_per_characters, sorted_dictionary
 
-file_path = "./books/frankenstein.txt"
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1) 
+
+file_path = sys.argv[1] #"./books/frankenstein.txt"
 
 book = get_book(file_path)
+num_words = get_num_words(book)
+dict_num_words = sorted_dictionary(get_num_per_characters(book))
 
-dict_num_words = get_num_characters(book)
+print("============ BOOKBOT ============")
+print("Analyzing book found at books/frankenstein.txt...")
+print("----------- Word Count ----------")
+print(num_words)
+print("--------- Character Count -------")
 
-print(dict_num_words)
+for dictionary in dict_num_words:
+
+    letter = list(dictionary.keys())[0]
+    if letter.isalpha():
+        count = list(dictionary.values())[0]
+        print(f"{letter}: {count}")
+
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -5,23 +5,33 @@ def get_book(file_path):
 
     return book_text
 
-def get_num_words(file_path):
-
-    book_text = get_book(file_path)
+def get_num_words(book_text):
     num_words = len(book_text.split())
+    return f"Found {num_words} total words"
 
-    return print(f"{num_words} words found in the document")
-
-def get_num_characters(book_text):
+def get_num_per_characters(book_text):
 
     book_text = book_text.lower()
-
     dict_letters = {}
 
     for letter in book_text:
         if letter not in dict_letters:
             dict_letters[letter] = 0
-
         dict_letters[letter] += 1
 
     return dict_letters
+
+def sorted_dictionary(dict_letters):
+    lst_dict = []
+
+    for key in dict_letters:
+        temp_dict = {}
+        temp_dict[key] = dict_letters[key]
+        lst_dict.append(temp_dict)
+
+    def sort_on(dict_item):
+        return list(dict_item.values())[0]
+
+    lst_dict.sort(key=sort_on, reverse=True)
+
+    return lst_dict
